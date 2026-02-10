fecd78918547e2a4b0bead691d4f8043f895976e
CarlesLlaurado <carlesllauradollaurado@gmail.com>
2026-02-10 15:17:56 +0100

    10/02/2026

--- Week_2/src/arrays/exercises/Exercise2.java
+++ Week_2/src/arrays/exercises/Exercise2.java
@@ -13,5 +13,63 @@ public class Exercise2
     public static void main(String[] args)
     {
         // TODO: Implement exercise
+
+        int[][] matrix = {
+                {1, 2, 3},
+                {2, 3, 4},
+                {7, 8, 9}
+        };
+
+//        System.out.print(matrix[2][0] + " ");
+//        System.out.print(matrix[2][1] + " ");
+//        System.out.print(matrix[2][2] + " ");
+//
+//        System.out.println();
+//
+//        for (int i = 0; i < 3; i++)
+//        {
+//            for (int j = 0; j < 3; j++)
+//            {
+//                System.out.print(matrix[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+
+        char[][] chessBoard = new char[8][8];
+
+        for (int i = 0; i < 8; i++)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                if (i % 2 == 0)
+                {
+                    if (j % 2 == 0)
+                    {
+                        chessBoard[i][j] = '#';
+                    }
+                    else{
+                        chessBoard[i][j] = ' ';
+                    }
+                }
+                else
+                {
+                    if (j % 2 != 0)
+                    {
+                        chessBoard[i][j] = '#';
+                    }
+                    else
+                    {
+                        chessBoard[i][j] = ' ';
+                    }
+                }
+            }
+        }
+        for (int q = 0; q < 8; q++) {
+            for (int k = 0; k < 8; k++) {
+                System.out.print(chessBoard[q][k]);
+            }
+            System.out.println();
+        }
     }
+
 }
--- Week_2/src/classes/exercises/Exercise1.java
+++ Week_2/src/classes/exercises/Exercise1.java
@@ -8,11 +8,34 @@ package classes.exercises;
  * 2. Add a method `displayInfo()` to print book details.
  * 3. Create objects in main() and call the method.
  */
+
+class Book {
+
+    String title;
+    String author;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+    public void displayInfo() {
+        System.out.println("Libro: " + this.title + " | Escrito por: " + this.author);
+
+    }
+}
+
+
 public class Exercise1
 {
     public static void main(String[] args)
     {
         // TODO: Create Book objects and test methods
+
+        Book myBook1 = new Book("El Quijote", "Cervantes");
+        Book myBook2 = new Book("Rich Dad, Poor dad", "Robert Kyosaky");
+
+        myBook1.displayInfo();
+        myBook2.displayInfo();
     }
 }
 
